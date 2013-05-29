7ef66ac4e4851cea2b60dfc1c4773e87758e954a
Gerrit <gvanvuuren@Gerrits-iMac.local>
2013-05-29 10:48:42 +0200

    add error handling to worker

--- pseidon/src/pseidon/core/worker.clj
+++ pseidon/src/pseidon/core/worker.clj
@@ -2,6 +2,7 @@
 (use '[clojure.tools.logging])
 (use '[pseidon.core.queue :as q])
 (use '[pseidon.core.registry :as r])
+(use '[pseidon.core.watchdog :as w])
 
 ;This module contains methods that help with the worker delegation from the queue and consumption.
 
@@ -12,14 +13,13 @@
 
 ;delagate to workers
 (defn delegate-msg [msg]
-  (try 
-    (doseq [topic (.-topics msg)]
-      (process-msg (r/reg-get topic) msg)
-     )    (catch Exception e (error "ERROR @TO FIX implement an error handling sink " e)))
-  )
+    (let [topic (:topic msg)
+          exec (r/reg-get topic)]
+     (exec msg)   
+  ))
 
 (defn start-consume [channel]
   "Start consuming from the channel this method runs async"
-   (consume channel (submit delegate-msg))
+   (consume channel (submit (w/watch-critical-error delegate-msg) ))
    true
   )
