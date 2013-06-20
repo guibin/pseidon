5ae4c3e0a952a3dba1cdb1c0dba1123127d32a22
Gerrit <gvanvuuren@Gerrits-iMac.local>
2013-06-20 16:45:53 +0200

    add bytes

---
+++ pseidon/java/pseidon/util/Bytes.java
@@ -0,0 +1,123 @@
+package pseidon.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * 
+ * Utility methods that convert between numeric bytes
+ * 
+ */
+public final class Bytes {
+
+	public static final byte[] ZERO = toBytes(0);
+
+	public static final byte[] toBytes(final byte[] v) {
+		return v;
+	}
+
+
+	public static final long size(File file){
+		return file.length();
+	}
+	
+	public static final void write(OutputStream out, byte[] bts) throws IOException {
+	    out.write(bts, 0, bts.length);
+	}
+
+	public static final byte[] toBytes(final String v)
+			throws UnsupportedEncodingException {
+		return v.getBytes("utf-8");
+	}
+
+	public static final byte[] toBytes(final long v) {
+		return new byte[] { (byte) (0xff & (v >> 56)),
+				(byte) (0xff & (v >> 48)), (byte) (0xff & (v >> 40)),
+				(byte) (0xff & (v >> 32)), (byte) (0xff & (v >> 24)),
+				(byte) (0xff & (v >> 16)), (byte) (0xff & (v >> 8)),
+				(byte) (0xff & v) };
+	}
+
+	public static final byte[] toBytes(final int v) {
+		return new byte[] { (byte) (0xff & (v >> 24)),
+				(byte) (0xff & (v >> 16)), (byte) (0xff & (v >> 8)),
+				(byte) (0xff & v) };
+	}
+
+	/**
+	 * Read the number from bts (this can be a 4 length int or a 8 length long)
+	 * and returns the incremented value.
+	 * 
+	 * @param bts
+	 * @return
+	 */
+	public static final byte[] inc(final byte[] bts, final int v) {
+		if (bts.length == 4) {
+			return toBytes(toInt(bts) + v);
+		} else if (bts.length == 8) {
+			return toBytes(toLong(bts) + v);
+		} else
+			throw new RuntimeException(
+					"The byte array should be length 4 or 8 and not "
+							+ bts.length + " possible value " + new String(bts));
+	}
+
+	public static final byte[] dec(final byte[] bts, final int v) {
+		return inc(bts, v * -1);
+	}
+
+	/**
+	 * Read the number from bts (this can be a 4 length int or a 8 length long)
+	 * and returns the incremented value.
+	 * 
+	 * @param bts
+	 * @return
+	 */
+	public static final byte[] inc(final byte[] bts, final long v) {
+		if (bts.length == 4) {
+			return toBytes(toInt(bts) + v);
+		} else if (bts.length == 8) {
+			return toBytes(toLong(bts) + v);
+		} else
+			throw new RuntimeException(
+					"The byte array should be length 4 or 8 and not "
+							+ bts.length + " possible value " + new String(bts));
+	}
+
+	public static final byte[] dec(final byte[] bts, final long v) {
+		return inc(bts, v * -1);
+	}
+
+	public static final String toString(final byte[] bts)
+			throws UnsupportedEncodingException {
+		if (bts == null)
+			return "";
+		else
+			return new String(bts, "utf-8");
+	}
+
+	public static final int toInt(final byte[] bts) {
+		if (bts == null || bts.length == 0)
+			return 0;
+		else
+			return (((bts[0] & 0xff) << 24) | ((bts[1] & 0xff) << 16)
+					| ((bts[2] & 0xff) << 8) | (bts[3] & 0xff));
+	}
+
+	public static final long toLong(final byte[] bts) {
+		if (bts == null || bts.length == 0)
+			return 0L;
+		else if (bts.length == 4)
+			return (long) toInt(bts);
+		else
+			return (((long) (bts[0] & 0xff) << 56)
+					| ((long) (bts[1] & 0xff) << 48)
+					| ((long) (bts[2] & 0xff) << 40)
+					| ((long) (bts[3] & 0xff) << 32)
+					| ((long) (bts[4] & 0xff) << 24)
+					| ((long) (bts[5] & 0xff) << 16)
+					| ((long) (bts[6] & 0xff) << 8) | ((long) (bts[7] & 0xff)));
+	}
+}
