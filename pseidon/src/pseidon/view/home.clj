3e30bd079b3ba4b364e6de660f95e87d1fa77138
Gerrit <gerritjvv@gmail.com>
2013-09-20 11:38:34 +0200

    update release

---
+++ pseidon/src/pseidon/view/home.clj
@@ -0,0 +1,19 @@
+(ns pseidon.view.home)
+
+
+
+(defn home-index [req]
+  """
+     <html>
+     <body>
+      <a href=\"tracking\">Tracking</a><br/>
+      <a href=\"metrics\">Metrics</a><br/>
+      <a href=\"registry\">Registry</a><br/>
+      <p/>
+      For more information see:<br/>
+      <a href=\"https://github.com/gerritjvv/pseidon/tree/master/pseidon\"> pseidon github</a><br/>
+      
+     </body>
+     </html>
+     """
+  )
