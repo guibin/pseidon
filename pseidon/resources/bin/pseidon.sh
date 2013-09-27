6041f6664388cef438b2bcd070b51bd62a6a9803
Gerrit <gerritjvv@gmail.com>
2013-09-27 18:46:58 +0200

    add memory params

--- pseidon/resources/bin/pseidon.sh
+++ pseidon/resources/bin/pseidon.sh
@@ -29,7 +29,7 @@ JAVA=$JAVA_HOME/bin/java
 
 
 if [ -z $JAVA_HEAP ]; then
-export JAVA_HEAP="-Xmx2048m"
+export JAVA_HEAP="-Xmx4096m -Xms1024m"
 fi
 
 # check envvars which might override default args
@@ -54,4 +54,4 @@ fi
 CLASSPATH="$CONF_DIR:$CONF_DIR/META_INF:$cp"
 
 echo $CLASSPATH
-$JAVA -XX:MaxDirectMemorySize=2048M -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:SurvivorRatio=6 -XX:NewRatio=3 -XX:+DisableExplicitGC $JAVA_HEAP $JAVA_OPTS -Djava.library.path="$STREAMS_HOME/lib/native/Linux-amd64-64/" -classpath "$CLASSPATH" $CLIENT_CLASS $@
+$JAVA -server -XX:MaxDirectMemorySize=2048M -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:SurvivorRatio=8 -XX:NewRatio=3 -XX:+DisableExplicitGC $JAVA_HEAP $JAVA_OPTS -Djava.library.path="$STREAMS_HOME/lib/native/Linux-amd64-64/" -classpath "$CLASSPATH" $CLIENT_CLASS $@
