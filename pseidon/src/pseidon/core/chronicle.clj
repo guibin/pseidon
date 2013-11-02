69c5ef1ee98fc40efc31006f7ace5e502d7d2451
Gerrit Jansen van Vuuren <gvanvuuren@Gerrits-MacBook-Air.local>
2013-11-03 00:28:22 +0100

    add chronicle blocking non blocking methods

--- pseidon/src/pseidon/core/chronicle.clj
+++ pseidon/src/pseidon/core/chronicle.clj
@@ -1,10 +1,15 @@
 (ns pseidon.core.chronicle
-  (:require [clojure.core.async :refer [go chan >!! <!! close! <! >!]]
+  (:require [clojure.core.async :refer [go chan alts!! >!! <!! close! <! >! timeout]]
+            [clojure.java.io :refer [file]]
+            [clojure.tools.logging :refer [info error]]
             [pseidon.core.utils :refer [not-interrupted]])
   (:import [net.openhft.chronicle IndexedChronicle ChronicleConfig]
            [java.util.concurrent.atomic AtomicInteger]
+           [org.apache.commons.io FileUtils]
            [java.io File])
   )
+
+(declare write-to-chronicle)
 
 (defn path-to-str [path]
   (if (instance? File path)
@@ -12,51 +17,99 @@
     path))
 
 (defprotocol ChronicleQueue 
-  (offer [this msg])
+  (offer! [this msg])
+  (offer [this msg timeout-msg])
   (close [this])
-  (get [this]))
+  (poll! [this])
+  (poll [this timeout-ms]))
 
-(defrecord ChronicleQueueImpl [write-ch read-ch]
+(defrecord ChronicleQueueImpl [write-ch read-ch chronicle-ref]
   ChronicleQueue
-  (offer [this msg]
+  (offer! [this msg]
     (>!! write-ch msg))
-  
-  (get [this]
-    ))
+  (offer [this msg timeout-ms]
+    "Blocks till the message can be placed on the queue and returns true otherwise returns false"
+    (not 
+       (nil? 
+         (first (alts!! [(go (>! write-ch msg) msg) (timeout timeout-ms)])))))
 
+  (poll! [this]
+    "Poll for a value and blocks untill a value is available"
+    (<!! read-ch))
+  (poll [this timeout-ms]
+    "Retursn nil if timeout otherwise return a value"
+    (let [[v _] (alts!! [read-ch (timeout timeout-ms)])]
+          v))
+          
+  (close [this]
+    (let [{:keys [chronicle appender tailer]} @chronicle-ref]
+      (.close appender)
+      (.close tailer)
+      (.close chronicle)))
+    )
+
+(defn ^File get-latest-chronicle-dir [parent-dir]
+  "Takes as argument the parent directory of the chronicle indexes and looks for the 
+   latest directory"
+  (last
+    (sort-by #(.lastModified %) (file-seq (file parent-dir)))))
+
+(defn load-chronicle-path [path]
+  (get-latest-chronicle-dir path))
+    
+(defn ^String new-chronicle-path [path]
+  (str (path-to-str path) "/" (System/currentTimeMillis) "/queue"))
+           
 (defn ^IndexedChronicle create-chronicle [path]
     (let [ chronicle (IndexedChronicle. (path-to-str path) (ChronicleConfig/SMALL))
            appender (.createAppender chronicle)
            tailer (.createTailer chronicle)]
-      {:chronicle chronicle :appender appender :tailer tailer}))
+      (info "Creating chronicle dir " path  )
+      {:chronicle chronicle :appender appender :tailer tailer :queue-path path}))
         
 (defn block-on-size [^AtomicInteger size limit]
-  (while (>=  (.get size) limit)
-    (Thread/sleep 200)))
+  (while (> (.get size) limit)
+    (do 
+      ;(info "blocking in write: " (.get size))
+      (Thread/sleep 200))))
 
-(defn copy-ch [read-ch new-chronicle]
-  (let [[v ch] (alts!! [c (timeout 200)])]
+(defn copy-ch [read-ch new-chronicle i]
+  "Copies from the read channel to the new chronicle
+   retrns the amount of records copied"
+  (let [[v ch] (alts!! [read-ch (timeout 200)])]
     (if (= ch read-ch)
       (do 
-        (write-to-chronicle new-appender v)
-        (recur read-ch new-chronicle)))))
+        ;(info "copy " i)
+        (write-to-chronicle new-chronicle v)
+        (recur read-ch new-chronicle (inc i) ))
+        i)))
 
-(defn check-roll-chronicle! [chronicle-ref path read-ch ^AtomicInteger queue-size ^long segment-limit]
-  (if (> (.get queue-size) segment-limit)
+(defn check-roll-chronicle! [chronicle-ref path read-ch ^AtomicInteger queue-size ^AtomicInteger segment-size segment-limit]
+  "Check if we should roll the chronicle file
+   If so this function will create a new chronicle, copy any data left in the previous chronicle and deletes the previous"
+  (if (> (.get segment-size) segment-limit)
     (do 
-      (let [{:keys [chronicle appender tailer]} @chronicle-ref
-            new-chronicle (create-chronicle path)]
+      (let [{:keys [chronicle appender tailer queue-path]} @chronicle-ref
+            new-chronicle (-> path (new-chronicle-path) (create-chronicle))]
         ;we must read all left over messages from the old channel into the new one
-        (copy-ch read-ch new-chronicle)
-        (prn "closing !!!!")
-        (.set queue-size 0)
-        (.close appender)
-        (.close tailer)
-        (.close chronicle))
-      
-      (dosync (ref-set chronicle-ref new-chronicle)))))
+        
+        (let [len (copy-ch read-ch new-chronicle 0)]
+          (.set segment-size len)
+          (.set queue-size len))
+        
+        (future
+          ;do cleanup in a separate thread
+	        (.close appender)
+	        (.close tailer)
+	        (.close chronicle)
+	        (-> queue-path (file ) (.getParentFile) (FileUtils/deleteDirectory)))
+	        
+        (info "delete dir : " (-> queue-path (file ) (.getParentFile)))
+        (dosync (ref-set chronicle-ref new-chronicle))))))
+
     
 (defn write-to-chronicle [{:keys [^IndexedChronicle chronicle appender tailer]} ^bytes msg]
+  "Write the bytes message to the chronicle appender"
   (doto 
     appender
     .startExcerpt
@@ -65,33 +118,46 @@
     .finish))
  
  (defn ^bytes read-from-chronicle [tailer]
+   "Read a bytes message from tailer"
    (let [size (.readInt tailer)
          ^bytes bts (byte-array size)]
      (.read tailer bts)
      bts))
 
-(defn create-queue [path limit & {:keys [segment-limit] :or {:segment-limit 1000000}}]
-  (let [write-ch (chan)
+(defn create-queue [path limit & {:keys [segment-limit] :or {segment-limit (* 2 limit)}}]
+  "Returns a ChronicleQueue with background writters and readers enabled"
+  (let [segment-limit2 (if (> segment-limit limit) segment-limit (do (info "segment limit cannot be smaller than the limit setting to 2 * limit") 
+                                                                   (* 2 limit)))
+        write-ch (chan)
         read-ch (chan)
-        chronicle-ref (ref (create-chronicle path))
+        chronicle-ref (ref (if-let [p (load-chronicle-path path)] (create-chronicle p) (create-chronicle (new-chronicle-path path)  )))
         queue-size (AtomicInteger.)
+        segment-size (AtomicInteger.)
         ]
     (go 
       (while (not-interrupted)
         
         (let [^bytes msg (<! write-ch)]
-          (prn "writing " (.get queue-size))
+          ;(info "on write-ch " (String. msg))
+          (.incrementAndGet queue-size) ;the current queue size
+          (.incrementAndGet segment-size) ; count before rolling on a segment
+          
           (block-on-size queue-size limit)
-          (check-roll-chronicle! chronicle-ref path queue-size segment-limit)
-          (.incrementAndGet queue-size)
+          ;(info "after block-on-size on write-ch " (String. msg))
+          
+          (check-roll-chronicle! chronicle-ref path read-ch queue-size segment-size segment-limit2)
+          
           (write-to-chronicle @chronicle-ref msg))))
+    
     (go (while (not-interrupted)
         (let [{tailer :tailer} @chronicle-ref]
           (loop [t tailer]
             (if (.nextIndex t)
-              (read-from-chronicle tailer))
+              (do 
+                (>! read-ch (read-from-chronicle tailer))
+                (.decrementAndGet queue-size)))
             (recur t)))))
           
     ; ChronicleQueueImpl [write-ch read-ch]
-    (ChronicleQueueImpl. write-ch read-ch)))
+    (ChronicleQueueImpl. write-ch read-ch chronicle-ref)))
 
--- pseidon/test/pseidon/test/core/chronicle_test.clj
+++ pseidon/test/pseidon/test/core/chronicle_test.clj
@@ -1,23 +1,65 @@
 (ns pseidon.test.core.chronicle-test
   (:require [pseidon.test.core.utils :refer [create-tmp-dir]]
-            [pseidon.core.chronicle :refer [offer close create-queue]])
+            [pseidon.core.chronicle :refer [offer poll poll! offer! close create-queue]])
   (:use midje.sweet
         pseidon.core.chronicle))
 
 
 (facts "Test chronicle queue implementation"
-       
-       (fact "Test offer"
+      
+      (fact "Test offer and get no limit or segment overflow"
              
-             (let [limit 100000
-                   path (create-tmp-dir "chronicle" :delete-on-exit false)
-                   q (create-queue path limit :segment-limit 100)] 
+             (let [limit 10000
+                   path (create-tmp-dir "chronicle" :delete-on-exit true)
+                   q (create-queue path limit :segment-limit (* limit 2))] 
                (doall
-                 (dotimes [i limit]
-                   (offer q (.getBytes (str "msg " i)))))
+                 (dotimes [i 100]
+                   (dotimes [n 100]
+                     (offer! q (.getBytes (str "msg " i "-" n))))))
+                 
                (close q)
-               true => true
-               )
+                (let [read-queue (create-queue path limit)]
+                  (dotimes [i 100]
+                    (dotimes [n 100]
+                      (String. (poll! read-queue)) => (str "msg " i "-" n)))
+                  (close read-queue)))
              
+             )
+       (fact "Test offer write block on limit "
+             (let [limit 10
+                   path (create-tmp-dir "chronicle" :delete-on-exit true)
+                   q (create-queue path limit)]
+               (doall
+                 (dotimes [i 11]
+                   (offer! q (.getBytes (str "msg-" i)))))
+               ;the following should block,timeout and return false
+               (offer q (.getBytes (str "abc")) 100) => false 
+               
              ))
+       
+         (fact "Test offer write block on limit read the recover from limit block again"
+             (let [limit 10
+                   path (create-tmp-dir "chronicle" :delete-on-exit true)
+                   q (create-queue path limit)]
+               (doall
+                 (dotimes [i 11]
+                   (offer! q (.getBytes (str "msg-" i)))))
+               ;the following should block,timeout and return false
+               (offer q (.getBytes (str "abc")) 100) => false 
+               ;read two items
+               (doall
+                 (dotimes [i 3]
+                   (not (nil? (poll q 100))) => true))
+               
+               ;write twice no blocking
+               (doall
+                 (dotimes [i 2]
+                   (prn "write " i)
+                   (offer q (.getBytes "msg") 100) => true))
+               
+               ;block again
+               (offer q (.getBytes (str "abc")) 100) => false 
+             ))
+
+       )
 
