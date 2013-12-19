92b122e4a6b295734d6cebfc29541c188c2b4930
gerritjvv <gerritjvv@gmail.com>
2013-12-19 11:57:43 +0100

    add view

--- pseidon/src/pseidon/view/metrics.clj
+++ pseidon/src/pseidon/view/metrics.clj
@@ -1,9 +1,9 @@
 (ns pseidon.view.metrics
   (:require 
-            [pseidon.core.metrics :refer [list-metrics]]
+            [pseidon.core.metrics :refer [list-metrics run-health-checks]]
             [pseidon.view.utils :refer [write-json]]
             )
   )
 
 (defn metrics-index [req]
-  (write-json (list-metrics)))
+  (write-json [ (list-metrics) (run-health-checks) ] ))
